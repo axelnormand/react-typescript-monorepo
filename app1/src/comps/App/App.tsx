9955598ea345cac9e0025e55dbbf8caeec95ed7e
axelnormand <alex.norman@alphakinetic.com>
2018-10-18 13:41:43 +0100

    use h1

--- app1/src/comps/App/App.tsx
+++ app1/src/comps/App/App.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 const App = () => {
   return (
     <>
-      <Header>Welcome to Monorepo Webapp 1!</Header>
+      <h1>Welcome to Monorepo Webapp 1!</h1>
       <InfoText>{bar()}</InfoText>
     </>
   );
--- app2/src/comps/App/App.tsx
+++ app2/src/comps/App/App.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 const App = () => {
   return (
     <>
-      <Header>Welcome to Monorepo Webapp 2!</Header>
+      <h1>Welcome to Monorepo Webapp 2!</h1>
       <InfoText>{bar()}</InfoText>
     </>
   );
